2307a313cfad61d6631f08211eb9f22ad918ae5c
Peter Romijn <498883@edu.rocmn.nl>
2017-07-15 12:09:57 +0200

    Added grid

--- view/not-loggedin.php
+++ view/not-loggedin.php
@@ -1,2 +1,4 @@
-<h2 class="col-6">U bent niet ingelogd</h2>
-<p>Klik <a href="<?php echo $GLOBALS['config']['base_url'] ?>">hier</a> om in te loggen</p>
+<div class="col-6">
+  <h2>U bent niet ingelogd</h2>
+  <p>Klik <a href="<?php echo $GLOBALS['config']['base_url'] ?>">hier</a> om in te loggen</p>
+</div>
