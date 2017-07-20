864b0b18da33d957c79511dbe2061a8f9078b48b
Peter Romijn <498883@edu.rocmn.nl>
2017-07-20 16:08:46 +0200

    Added error handeling

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
   require_once 'config.php';
   require_once 'model/Router.class.php';
 
